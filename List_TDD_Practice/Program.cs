b2cc41cf4c054f2dfbc96952f1efceddf60072d9
Corey T Cox <coxcorey19@gmail.com>
2020-05-20 12:03:50 -0400

    added line 8 empty list

--- List_TDD_Practice/Program.cs
+++ List_TDD_Practice/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace List_TDD_Practice
 {
     class Program
     {
+        static void emptyList(); 
         static void Main(string[] args)
         {
             Console.WriteLine("Let's practice creating tests for Lists!");
